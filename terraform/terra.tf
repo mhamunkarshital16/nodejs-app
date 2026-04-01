eeb796f5742faeaafbcfe31c302c048421e256cd
Ubuntu <ubuntu@ip-172-31-9-224.ap-south-1.compute.internal>
2026-04-01 19:47:31 +0000

    added security group & allows traffic for port 3000 and 22

--- terraform/terra.tf
+++ terraform/terra.tf
@@ -2,21 +2,46 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+resource "aws_security_group" "node_sg" {
+    name = "node-sg"
+    description = "Allow Node.js app traffic"
+
+    ingress {
+      description = "Allow HTTP (app port)"
+      from_port = 3000
+      to_port = 3000
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+       description = "Allow SSH"
+       from_port = "22"
+       to_port = "22"
+       protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+    }
+
+}
+
+
 resource "aws_instance" "node_app" {
   ami           = "ami-0f5ee92e2d63afc18" # Example Amazon Linux
   instance_type = "t3.micro"
 
   key_name = "test-app_key"
 
+  vpc_security_group_ids = aws_security_group.node_sg.id
+
   user_data = <<-EOF
               #!/bin/bash
-              yum update -y
-              yum install docker -y
-              service docker start
+              sudo apt update -y
+              sudo apt install docker.io -y
+              sudo usermod -aG docker $USER && newgrp docker
               docker run -d -p 3000:3000 node-js:v1
               EOF
 
   tags = {
-    Name = "NodeAppServer"
+    Name = "NodeAppServer-1"
   }
 }
